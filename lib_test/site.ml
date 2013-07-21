fdca6e665e39c458c940a5688425df5c204e72e0
Anil Madhavapeddy <anil@recoil.org>
2013-07-21 20:12:15 +0100

    add some test cases based on the mirage website

--- lib_test/site.ml
+++ lib_test/site.ml
@@ -1,21 +1,36 @@
 open Cowabloga
+open Lwt
 
-let post1 =
-  let title = "Blog Post 1", Uri.of_string "/post1" in
-  let author = "Anil Madhavapeddy", (Uri.of_string "http://anil.recoil.org") in
-  let content = <:html<<p>Florble</p><p>Wibble</p>&>> in
-  let date = <:html<12th August 2013>> in
-  Blog_template.post ~title ~author ~date ~content
+let read_entry ent =
+  match Mirage_entries.read ent with
+  | None -> return <:html<$str:"???"$>>
+  | Some b ->
+    let md = Cow.Markdown.of_string b in
+    return (Cow.Markdown.to_html md)
+
+let config = { 
+  Blog.base_uri="http://localhost:8081";
+  title="Blog";
+  subtitle=Some "musings on OpenMirage development";
+  rights=Mirage_people.rights;
+  read_entry
+}
+
+let posts =
+  Lwt_unix.run (Blog.entries_to_html config Mirage_blog.entries)
 
 let t =
-  let title = <:html<My test website>> in
   let uri = Uri.of_string in
-  let nav_links = ["home", uri "/"; "blog", uri "/blog"; "contact", uri "/contact"] in
+  let nav_links = [
+    "home",    uri "/";
+    "blog",    uri "/blog";
+    "contact", uri "/contact" ]
+  in
   let side_links = nav_links in
-  let posts = [post1] in
   let copyright = <:html<Anil Madhavapeddy>> in
-  Blog_template.t ~title ~nav_links ~side_links ~posts ~copyright ()
+  let { Blog.title; subtitle } = config in
+  Blog_template.t ~title ~subtitle ~nav_links ~side_links ~posts ~copyright ()
 
 let blog =
-  let body = Foundation.body ~title:"My Blog" ~content:t in
+  let body = Foundation.body ~title:"Mirage Musings" ~content:t in
   Foundation.page ~body
