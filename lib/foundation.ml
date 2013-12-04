ab114673a71267b7a010f526697001c337e94238
Richard Mortier <mort@cantab.net>
2013-12-04 00:24:50 +0000

    [lib] use minified `foundation.css`

--- lib/foundation.ml
+++ lib/foundation.ml
@@ -21,7 +21,7 @@ let body ~title ~content =
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width"/>
   <title>$str:title$</title>
-  <link rel="stylesheet" href="css/foundation.css"> </link>
+  <link rel="stylesheet" href="css/foundation.min.css"> </link>
   <link rel="stylesheet" href="css/site.css"> </link>
   <script src="js/vendor/custom.modernizr.js"> </script>
 </head>
