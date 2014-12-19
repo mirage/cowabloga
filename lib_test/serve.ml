415ef5d091c031bb7c41b9ba888d00aacec23fb7
Anil Madhavapeddy <anil@recoil.org>
2014-12-18 16:16:50 -0800

    tweak the Server.make for cohttp.0.14.0

--- lib_test/serve.ml
+++ lib_test/serve.ml
@@ -33,7 +33,7 @@ let make_server () =
   let conn_closed (_,conn_id)=
     Printf.eprintf "conn %s closed\n%!" (Connection.to_string conn_id);
   in
-  let config = Server.make ~callback  ~conn_closed in
+  let config = Server.make ~callback  ~conn_closed () in
   let ctx = Cohttp_lwt_unix_net.init () in
   Server.create ~ctx ~mode:(`TCP (`Port 8081)) config
 
